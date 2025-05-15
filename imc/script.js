2fce30988a083b526c4b6739657b4eb106f95de3
Lucas Souza Santos <00001118720532sp@aluno.educacao.sp.gov.br>
2025-05-15 15:26:39 -0300

    projetoIMC

---
+++ imc/script.js
@@ -0,0 +1,7 @@
+let $nome = document.getElementById('nome')
+let $altura = document.getElementById('altura')
+let $peso = document.getElementById('peso')
+let $btnCalcular = document.getElementById('calcular')
+let $valor = document.getElementById('valor')
+
+alert($some)
